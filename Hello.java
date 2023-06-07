beea8a3abba7d6b11c87106953c9f48166cbe3eb
Gagik Hovhannisyan <hovhaisyangagik@gmail.com>
2023-06-07 12:03:56 +0400

    Hello

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+
+	public static void main(String[] args){
+		String firstName = "Gagik";
+		String lastName = "Hovhannisyan";
+		
+		System.out.println(14.1 + 11);
+		System.out.println(14 * 6);
+		System.out.println(firstName +" " +  lastName);
+		
+
+	}
+
+
+}
