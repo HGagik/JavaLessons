73b8a42b8048f2cb05542075f9c6b0cb8f601fdc
Gagik Hovhannisyan <hovhaisyangagik@gmail.com>
2023-06-10 10:04:17 +0400

    Second homework

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays{
+	 public static void main(String[] args) {
+
+
+ String [] names = {"Anahit","Tatev","Irina","Gagik","Elen","Anna"};
+ int [] ages = {21,24,26,16,30,34};
+ System.out.println("Name is " + names[2] + " age is " + ages[2]);
+
+	}
+
+ }
